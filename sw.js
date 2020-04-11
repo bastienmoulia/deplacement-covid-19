eea6933a2c62335fb58071433e8be816fb27e39a
bastienmoulia <bastienmoulia@users.noreply.github.com>
2020-04-11 16:50:25 +0000

    Deploy to GitHub pages

---
+++ sw.js
@@ -0,0 +1,117 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "android-chrome-192x192.0fcebb82.png",
+    "revision": "71c32dc8b4da4840d61a1796a2bc230b"
+  },
+  {
+    "url": "android-chrome-512x512.ae40f73d.png",
+    "revision": "8bf8012c4f4ae251d660b4739171b7ed"
+  },
+  {
+    "url": "apple-touch-icon-120x120-precomposed.044c6f3f.png",
+    "revision": "a6e447e90b691da286286ab18849adac"
+  },
+  {
+    "url": "apple-touch-icon-120x120.044c6f3f.png",
+    "revision": "a6e447e90b691da286286ab18849adac"
+  },
+  {
+    "url": "apple-touch-icon-precomposed.fdeabd24.png",
+    "revision": "de5e22b15cf20cb0d586a036d714ee3f"
+  },
+  {
+    "url": "apple-touch-icon.fdeabd24.png",
+    "revision": "de5e22b15cf20cb0d586a036d714ee3f"
+  },
+  {
+    "url": "certificate.038824ba.css",
+    "revision": "c1851b21d52d9f3f8da685f0a3f989f3"
+  },
+  {
+    "url": "certificate.84dda806.pdf",
+    "revision": "343ac8bb3542a6578f2a4cbaf0f99679"
+  },
+  {
+    "url": "confidentialite.038824ba.css",
+    "revision": "6af134e8feeabe3faf6c40d00622b63c"
+  },
+  {
+    "url": "confidentialite.0b673004.js",
+    "revision": "1b853e29ef82f94df3c24eb3d25e74e8"
+  },
+  {
+    "url": "confidentialite.html",
+    "revision": "966d2a1bbe7bf48198182606f16abeca"
+  },
+  {
+    "url": "favicon-16x16.a4687270.png",
+    "revision": "652605ae0182d51781ab0be639ad2bda"
+  },
+  {
+    "url": "favicon-32x32.623384d0.png",
+    "revision": "674c9c4ef1e4c7ea9de1218ee0bfd0cf"
+  },
+  {
+    "url": "index.html",
+    "revision": "1694c7b3ecf67c81367f5d76aa827387"
+  },
+  {
+    "url": "logo_dnum.02e567b8.svg",
+    "revision": "79cf40d4bdaa3a5de3475d90800005cf"
+  },
+  {
+    "url": "marianne-bold-webfont.1505950c.woff2",
+    "revision": "e67f6cefe32cc39f909e605c8d6337a9"
+  },
+  {
+    "url": "marianne-bold-webfont.7424dbde.woff",
+    "revision": "0bcc99dd4adfb78e11098fedfe531cbb"
+  },
+  {
+    "url": "marianne-regular-webfont.0a959359.woff",
+    "revision": "89f4f2326c77429e98693cf83703face"
+  },
+  {
+    "url": "marianne-regular-webfont.daa94941.woff2",
+    "revision": "d2c09e5f58d8360f541e2a8726c33587"
+  },
+  {
+    "url": "MIN_Interieur_RVB.57f44120.svg",
+    "revision": "75f5937c93153461f08e8723cf937a12"
+  },
+  {
+    "url": "safari-pinned-tab.1551797e.svg",
+    "revision": "f53452e6ac8760f12bab91672e91d39b"
+  },
+  {
+    "url": "./",
+    "revision": "1a6c9806e3f51259c11d23539e221f31"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://bastienmoulia.github.io/deplacement-covid-19/index.html"));
